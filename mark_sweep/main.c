4d344ad0250d300b34ec4936f6616268f4431e52
Andres Masanelli <masanelli.a@gmail.com>
2025-10-06 13:34:21 +1100

    test

--- mark_sweep/main.c
+++ mark_sweep/main.c
@@ -5,21 +5,26 @@
 int main(int argc, char *argv[])
 {
   VirtualMachine *vm;
-  Frame *frame;
+  Frame *frame1;
+  Frame *frame2;
   Object *object;
 
   vm = new_vm();
-  frame = vm_new_frame(vm);
+  frame1 = vm_new_frame(vm);
+  frame2 = vm_new_frame(vm);
   object = new_string("hello!");
-  frame_reference_object(frame, object);
+  frame_reference_object(frame2, object);
+  frame_reference_object(frame1, object);
+  printf("%s\n", object->data.as_string);
 
   vm_collect_garbage(vm);
+  printf("%s\n", object->data.as_string);
 
   vm_frame_free(vm);
+  printf("%s\n", object->data.as_string);
 
   vm_collect_garbage(vm);
-
-  vm_free(vm);
+  printf("%s", object->data.as_string);
 
   return 0;
 }
