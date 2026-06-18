defb1c8cb9626de32b1494c99fd0d6155163f53a
Andres Masanelli <masanelli.a@gmail.com>
2026-06-18 22:13:28 +1000

    sleep asm

--- sleep/sleep_pure.asm
+++ sleep/sleep_pure.asm
@@ -8,6 +8,7 @@ _start:
     jne .usage
 
     mov rsi, [rsp+16]   /* argv[1] -- rsp+8=argv[0], rsp+16=argv[1] */
+    mov r8, rsi         /* save start of argv[1] to print it and compute length later */
 
     /* parse decimal integer from string into rbx */
     xor rbx, rbx
@@ -24,6 +25,31 @@ _start:
     jmp .parse
 
 .sleep:
+    /* rsi = end of digits, r8 = start -- compute length without int-to-string conversion */
+    mov r9, rsi
+    sub r9, r8          /* r9 = length of number string */
+
+    /* write "Sleeping for " */
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rip + sleeping_msg]
+    mov rdx, sleeping_msg_end - sleeping_msg
+    syscall
+
+    /* write the number (reuse argv[1] string directly) */
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r8
+    mov rdx, r9
+    syscall
+
+    /* write " seconds\n" */
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rip + seconds_msg]
+    mov rdx, seconds_msg_end - seconds_msg
+    syscall
+
     /* build timespec { tv_sec, tv_nsec } on stack */
     sub rsp, 16
     mov [rsp], rbx             /* tv_sec = parsed seconds */
@@ -56,6 +82,12 @@ _start:
     ud2
 
 .section .rodata
+sleeping_msg:
+    .ascii "Sleeping for "
+sleeping_msg_end:
+seconds_msg:
+    .ascii " seconds\n"
+seconds_msg_end:
 usage_msg:
     .ascii "Usage: sleep NUMBER\nPause for NUMBER seconds\n"
 usage_msg_end:
